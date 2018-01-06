a53f2549d758888cd119f8b5bcc5935373434d2b
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2018-01-07 04:57:02 +0530

    updated code :tada:

--- error_pages.php
+++ error_pages.php
@@ -1,5 +1,4 @@
 <?php
-    if(!file_exists("main.css")) echo "WARNING: main.css not found. Pages may not be able to display errors correctly.\n";
     if(!file_exists("error_list.xml")) die("ERROR: Unable to load error_list.xml\n"); 
     $errors = simplexml_load_file("error_list.xml");
     foreach ($errors->error as $error) {
@@ -7,7 +6,7 @@
 <html>
 <head>
     <title>Uh... 3RR0R!!!!!1</title>
-    <link rel='shortcut icon' href='favicon.ico' type='image/x-icon'>
+    <link rel='shortcut icon' href='//raw.githubusercontent.com/muhammadmuzzammil1998/error_pages/master/favicon.ico' type='image/x-icon'>
     <style>@import url(https://fonts.googleapis.com/css?family=Montserrat:300);div,span{text-shadow:0 1px 0 rgba(137,137,137,.5),0 2px 0 rgba(137,137,137,.5),0 3px 0 rgba(137,137,137,.5),0 4px 0 rgba(137,137,137,.5),0 5px 0 rgba(137,137,137,.5),0 6px 0 rgba(137,137,137,.5),0 7px 0 rgba(137,137,137,.5)}a:hover,body{color:#fff}*{margin:0}body{background:#000;font-family:Montserrat;text-align:center;font-size:100pt}div{margin-top:5%}div::before{content:attr(error)}span::before{content:attr(desc)}p:after{font-size:15%;content:attr(text)}a{color:grey;transition:all .5s;cursor:pointer;text-decoration:none;font-size:25%}a::before{content:attr(data)}</style>
 </head>
 <body>
